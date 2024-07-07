22b5f8d78af4a8375302e8f11727884d6f0995f1
Bruno <bruno.lima.dev@outlook.com>
2024-07-07 15:33:00 -0300

    first commit

---
+++ src/main/java/com/fiap/fiap_tc5_ecommerce_orchestrator_ms/fiap_tc5_ecommerce_orchestrator_ms/FiapTc5EcommerceOrchestratorMsApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.fiap_tc5_ecommerce_orchestrator_ms.fiap_tc5_ecommerce_orchestrator_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapTc5EcommerceOrchestratorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapTc5EcommerceOrchestratorMsApplication.class, args);
+	}
+
+}
