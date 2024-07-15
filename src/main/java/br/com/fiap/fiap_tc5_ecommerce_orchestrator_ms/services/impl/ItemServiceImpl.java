341740c6ba81dc6417cb3dc25c3d34a2f5fbf63c
Wiliam Nascimento <wilnasc990s3@gmail.com>
2024-07-15 18:12:11 -0300

    Refatorando ItemService e adicionando chamadas que estavam faltando

--- src/main/java/br/com/fiap/fiap_tc5_ecommerce_orchestrator_ms/services/impl/ItemServiceImpl.java
+++ src/main/java/br/com/fiap/fiap_tc5_ecommerce_orchestrator_ms/services/impl/ItemServiceImpl.java
@@ -11,12 +11,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ItemServiceImpl implements ItemService {
 
   private final RestTemplate restTemplate;
+
+  private static final String SESSION_ID_HEADER = "session-id";
 
   @Value("${microservices.item-ms.url}")
   private String itemMsUrl;
@@ -32,11 +36,17 @@ public class ItemServiceImpl implements ItemService {
 
     try {
 
-      this.restTemplate.postForEntity(itemMsUrl, itemModel, null)
-              .getHeaders()
-              .add("session-id", sessionId);
+      HttpHeaders headers = new HttpHeaders();
+      headers.set(SESSION_ID_HEADER, sessionId);
 
-      return itemModel;
+      HttpEntity<ItemModel> requestEntity = new HttpEntity<>(itemModel, headers);
+
+      ResponseEntity<ItemModel> response = restTemplate
+              .exchange(
+                      itemMsUrl, HttpMethod.POST, requestEntity, ItemModel.class
+              );
+
+      return response.getBody();
 
     } catch (RestClientException e) {
 
@@ -49,7 +59,26 @@ public class ItemServiceImpl implements ItemService {
 
   @Override
   public List<ItemModel> getItems(String sessionId) {
-    return null;
+
+    try {
+
+      HttpHeaders headers = new HttpHeaders();
+      headers.set(SESSION_ID_HEADER, sessionId);
+
+      HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+      ResponseEntity<ItemModel[]> response = restTemplate.exchange(
+              itemMsUrl, HttpMethod.GET, requestEntity, ItemModel[].class);
+
+      return Arrays.stream(Objects.requireNonNull(response.getBody())).toList();
+
+    } catch (RestClientException e) {
+
+      e.printStackTrace();
+      throw new RuntimeException("Erro ao validar usuário!");
+
+    }
+
   }
 
   @Override
@@ -58,14 +87,11 @@ public class ItemServiceImpl implements ItemService {
     try {
 
       HttpHeaders headers = new HttpHeaders();
-      headers.set("session-id", sessionId);
+      headers.set(SESSION_ID_HEADER, sessionId);
 
       HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
 
-      String url = new StringBuilder(itemMsUrl)
-              .append("/")
-              .append(id)
-              .toString();
+      String url = itemMsUrl + "/" + id;
 
       ResponseEntity<ItemModel> response = restTemplate.exchange(
               url, HttpMethod.GET, requestEntity, ItemModel.class);
@@ -83,12 +109,56 @@ public class ItemServiceImpl implements ItemService {
 
   @Override
   public ItemModel removeItem(Long id, String sessionId) {
-    return null;
+
+    try {
+
+      HttpHeaders headers = new HttpHeaders();
+      headers.set(SESSION_ID_HEADER, sessionId);
+
+      HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+      String url = itemMsUrl + "/" + id;
+
+      ResponseEntity<ItemModel> response = restTemplate.exchange(
+              url, HttpMethod.DELETE, requestEntity, ItemModel.class);
+
+      return response.getBody();
+
+    } catch (RestClientException e) {
+
+      e.printStackTrace();
+      throw new RuntimeException("Erro ao validar usuário!");
+
+    }
+
   }
 
   @Override
-  public ItemModel updateItem(Long id, ItemModel itemEntity, String sessionId) {
-    return null;
+  public ItemModel updateItem(Long id, ItemModel itemModel, String sessionId) {
+
+    try {
+
+      HttpHeaders headers = new HttpHeaders();
+      headers.set(SESSION_ID_HEADER, sessionId);
+
+      HttpEntity<ItemModel> requestEntity = new HttpEntity<>(itemModel, headers);
+
+      String url = itemMsUrl + "/" + id;
+
+      ResponseEntity<ItemModel> response = restTemplate
+              .exchange(
+                      url, HttpMethod.PUT, requestEntity, ItemModel.class
+              );
+
+      return response.getBody();
+
+    } catch (RestClientException e) {
+
+      e.printStackTrace();
+      throw new RuntimeException("Erro ao validar usuário!");
+
+    }
+
   }
 
 }
