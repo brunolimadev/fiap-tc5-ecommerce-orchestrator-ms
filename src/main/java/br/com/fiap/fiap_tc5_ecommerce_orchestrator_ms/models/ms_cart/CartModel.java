dfee4727cf5682a4f22778e2e9e5e4ad55c7e17f
Wiliam Nascimento <wilnasc990s3@gmail.com>
2024-07-15 20:07:19 -0300

    Corrigindo tipo de lista retornada no carrinho de compras

--- src/main/java/br/com/fiap/fiap_tc5_ecommerce_orchestrator_ms/models/ms_cart/CartModel.java
+++ src/main/java/br/com/fiap/fiap_tc5_ecommerce_orchestrator_ms/models/ms_cart/CartModel.java
@@ -1,11 +1,9 @@
 package br.com.fiap.fiap_tc5_ecommerce_orchestrator_ms.models.ms_cart;
 
-import br.com.fiap.fiap_tc5_ecommerce_orchestrator_ms.models.ms_item.ItemModel;
-
 import java.util.List;
 
 public record CartModel(
         Long id,
         Double totalOrder,
-        List<ItemModel>items
+        List<CartItemModel>items
 ) {}
